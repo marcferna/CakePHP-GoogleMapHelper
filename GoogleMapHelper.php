228a47078d3efef58f78a1cb2c3b75f4b1cc984b
Corie Slate <corie@houseoftech.com>
2014-11-16 10:15:32 +0100

    Remove unneccessary options parameter

--- GoogleMapHelper.php
+++ GoogleMapHelper.php
@@ -367,11 +367,10 @@ class GoogleMapHelper extends AppHelper {
   *
   * @author Corie Slate <corie.slate (at) gmail (dot) com>
   * @param $map_id - Id that you used to create the map (default 'map_canvas')
-  * @param array $options - options array
   * @return string - will return all the javascript script to add the clusterer to the map
   *
   */
-  function clusterMarkers($map_id, $options = array()){
+  function clusterMarkers($map_id){
     if($map_id == null) return null;
     
     $cluster = "<script>";
